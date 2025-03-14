0c4bf3e66d22b5a0df48ee04376a79860f488079
NNLisko <niko.1lausto2@gmail.com>
2025-03-14 22:11:11 +0200

    lutemon + assign hp + getters

--- app/src/main/java/com/example/lutemonnikomatei/Lutemon.java
+++ app/src/main/java/com/example/lutemonnikomatei/Lutemon.java
@@ -14,7 +14,7 @@ public class Lutemon {
     public Lutemon(String name, int speed, int maxhp, int attack) {
         this.name = name;
         this.speed = speed;
-        this.maxHp = maxhp;
+        this.maxHp = assignMaxHp();
         this.hp = maxhp;
         this.attack = attack;
         this.wins = 0;
@@ -31,5 +31,29 @@ public class Lutemon {
 
     public int getSpeed() {
         return this.speed;
+    }
+
+    public int getMaxHp() {
+        return this.maxHp;
+    }
+
+    public int getHp() {
+        return this.hp;
+    }
+
+    public LutemonTypes getType() {
+        return this.type;
+    }
+
+    public int getAttack() {
+        return this.attack;
+    }
+
+    private int assignMaxHp() {
+        int hp = 100;
+
+        int constant = (int) (Math.random() * 40) - 20;
+
+        return hp + constant;
     }
 }
