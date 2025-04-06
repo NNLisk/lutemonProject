b056247f272c64b40202e8b65670a7dd06f74986
rudeDoodle <110019148+rudeDoodle@users.noreply.github.com>
2025-04-06 16:55:57 +0300

    a

--- app/src/main/java/com/example/lutemonnikomatei/GUI/HomePage.java
+++ app/src/main/java/com/example/lutemonnikomatei/GUI/HomePage.java
@@ -12,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.lutemonnikomatei.LutemonManager;
 import com.example.lutemonnikomatei.R;
+import com.example.lutemonnikomatei.enums.LUTEMONTYPES;
 
 public class HomePage extends AppCompatActivity {
 
@@ -20,6 +21,8 @@ public class HomePage extends AppCompatActivity {
     Button testBattle;
     Button viewLute;
     Button trainLute;
+
+    Button testLutemonCreation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +35,21 @@ public class HomePage extends AppCompatActivity {
             return insets;
         });
 
+        lutemonManager.createLutemon("Niko", LUTEMONTYPES.STUDENT);
+        lutemonManager.createLutemon("Matei", LUTEMONTYPES.STUDENT);
+        lutemonManager.createLutemon("Cristian", LUTEMONTYPES.STUDENT);
+
+        lutemonManager.createLutemon("JANNE", LUTEMONTYPES.Janne);
+
+        lutemonManager.setPlayer1(lutemonManager.getListOfLutemons().get(3));
+        lutemonManager.setPlayer2(lutemonManager.getListOfLutemons().get(0));
+
         testBattle = findViewById(R.id.testBattle);
         viewLute = findViewById(R.id.viewLutemon);
+
         trainLute = findViewById(R.id.trainLutemon);
+
+        testLutemonCreation = findViewById(R.id.testCreateLutemon);
 
 
         testBattle.setOnClickListener(view -> {
@@ -44,8 +59,14 @@ public class HomePage extends AppCompatActivity {
         viewLute.setOnClickListener(view-> {Intent viewIntent = new Intent(HomePage.this, viewLutemons.class);
         startActivity(viewIntent);
         });
+
         trainLute.setOnClickListener(view-> {Intent viewIntent = new Intent(HomePage.this, Train.class);
             startActivity(viewIntent);
+        });
+
+        testLutemonCreation.setOnClickListener(view -> {
+            Intent createLutemonIntent = new  Intent(HomePage.this, CreateLutemon.class);
+            startActivity(createLutemonIntent);
         });
 
     }
